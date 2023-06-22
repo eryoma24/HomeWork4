d3aaba50ca762af949ef1686958c762314e241c3
NikolaySergeevich <kolya-2013@inbox.ru>
2023-06-22 21:18:37 +0600

    Finished hometasks

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+int number = ReadInt("Введите целое положительное число => ");
+int count = 1;
+while (count <= number)
+{
+    System.Console.WriteLine(count * count * count);
+    count ++;
+}

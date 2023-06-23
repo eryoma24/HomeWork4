3dd92b5453033a7c17f1f4254437a277eabc5354
NikolaySergeevich <kolya-2013@inbox.ru>
2023-06-23 18:18:48 +0600

    Found trouble

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int numbers5 = Convert.ToInt32(Console.ReadLine());
+    return numbers5;
+}
+int number = ReadInt("Введите пятизначное целое число => ");
+
+if ((number / 10000 == number % 10) && (number / 1000 % 10 == number / 10 % 10))
+{
+    System.Console.WriteLine($"{number} является палиндромом");
+}
+else{
+    System.Console.WriteLine($"{number} не является палиндромом");
+}
